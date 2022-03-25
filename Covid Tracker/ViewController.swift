78d144303505c68cf98f9f44899512a2f2d9b9fe
Pavel <pavel.kurzo@mail.ru>
2022-03-25 18:39:04 +0100

    update

---
+++ Covid Tracker/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Covid Tracker
+//
+//  Created by Павел Курзо on 24.03.22.
+//
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.groupingSeparator = ","
+        formatter.locale = .current
+        return formatter
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var dayData: [DayData] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.createGraph()
+            }
+        }
+    }
+    
+    private var scope: APICaller.DataScope = .national
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Covid Cases"
+        configureTable()
+        createFilterButton()
+        fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func createGraph() {
+        
+    }
+    
+    private func configureTable() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+    }
+
+    
+    private func fetchData() {
+        APICaller.shared.getCovidData(for: scope) { [weak self] result in
+            switch result {
+            case .success(let dayData):
+                self?.dayData = dayData
+            case .failure(let error):
+                print(error)
+            }
+            
+        }
+    }
+
+    private func createFilterButton() {
+        let buttonTitle: String = {
+            switch scope {
+            case .national: return "National"
+            case .state(let state): return state.name
+            }
+        }()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: buttonTitle,
+            style: .done,
+            target: self,
+            action: #selector(didTapFilter)
+        )
+    }
+
+    @objc private func didTapFilter() {
+        let vc = FilterViewController()
+        vc.completion = { [weak self] state in
+            self?.scope = .state(state)
+            self?.fetchData()
+            self?.createFilterButton()
+        }
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dayData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = dayData[indexPath.row]
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = createText(with: data)
+        return cell
+    }
+    
+    private func createText(with data: DayData) -> String {
+        let dateString = DateFormatter.prettyFormatter.string(from: data.date)
+        let total = Self.numberFormatter.string(from: NSNumber(value: data.count))
+        return "\(dateString): \(total ?? "\(data.count)")"
+    }
+}
+
